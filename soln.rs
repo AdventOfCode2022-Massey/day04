af15d860c99254f0dfb1ef6d0396cb27dc719504
Bart Massey <bart.massey@gmail.com>
2022-12-04 16:48:10 -0800

    finished part 2

--- soln.rs
+++ soln.rs
@@ -5,11 +5,55 @@
 //! Advent of Code Day 4.  
 //! Bart Massey 2022
 
+use std::ops::RangeInclusive;
+
 use aoc::*;
+use aoc_reparse::*;
+
+trait RangeInclusiveExt<Idx> {
+    fn contains_range(&self, other: &RangeInclusive<Idx>) -> bool;
+    fn laps_range(&self, other: &RangeInclusive<Idx>) -> bool;
+}
+
+impl<Idx> RangeInclusiveExt<Idx> for RangeInclusive<Idx>
+where
+    Idx: PartialOrd,
+{
+    fn contains_range(&self, other: &RangeInclusive<Idx>) -> bool {
+        self.start() >= other.start() && self.end() <= other.end()
+    }
+
+    fn laps_range(&self, other: &RangeInclusive<Idx>) -> bool {
+        self.contains(other.start()) || self.contains_range(other)
+    }
+}
 
 fn main() {
+    let parser = Reparse::new(r"([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)");
+    let input = input_lines().map(|line| {
+        let parsed = parser.parse(&line).unwrap();
+        let result: (RangeInclusive<u64>, RangeInclusive<u64>) = (
+            parsed.get(1)..=parsed.get(2),
+            parsed.get(3)..=parsed.get(4),
+        );
+        result
+    });
     match get_part() {
-        Part1 => todo!(),
-        Part2 => todo!(),
+        Part1 => {
+            let total = input
+                .filter(|(r1, r2)| {
+                    r1.contains_range(r2) || r2.contains_range(r1)
+                })
+                .count();
+            println!("{total}");
+        }
+        Part2 => {
+            let total = input
+                .filter(|(r1, r2)| {
+                    r1.laps_range(r2) || r2.laps_range(r1)
+                })
+                .count();
+            println!("{total}");
+        }
     }
 }
